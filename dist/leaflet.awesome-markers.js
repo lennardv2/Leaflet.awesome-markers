1232d91c9b4bcb66e11ce9c5943a49d00568c51c
K.K. POON <kkpoon@clustertech.com>
2014-03-28 14:47:29 +0800

    add text option for marker

--- dist/leaflet.awesome-markers.js
+++ dist/leaflet.awesome-markers.js
@@ -31,7 +31,8 @@
             extraClasses: '',
             icon: 'home',
             markerColor: 'blue',
-            iconColor: 'white'
+            iconColor: 'white',
+            font: 'monospace'
         },
 
         initialize: function (options) {
@@ -72,11 +73,21 @@
                 if(options.iconColor === 'white' || options.iconColor === 'black') {
                     iconColorClass = "icon-" + options.iconColor;
                 } else {
-                    iconColorStyle = "style='color: " + options.iconColor + "' ";
+                    iconColorStyle = "color: " + options.iconColor;
                 }
             }
+            if(options.font && options.text) {
+                if (iconColorStyle) {
+                    iconColorStyle += "; ";
+                }
+                iconColorStyle += "font-family: " + options.font;
+            }
+            
+            if (options.text) {
+                return "<i style='" + iconColorStyle + "' class='" + options.extraClasses + " " + options.prefix + " " + iconSpinClass + " " + iconColorClass + "'>" + options.text + "</i>";
+            }
 
-            return "<i " + iconColorStyle + "class='" + options.extraClasses + " " + options.prefix + " " + iconClass + " " + iconSpinClass + " " + iconColorClass + "'></i>";
+            return "<i style='" + iconColorStyle + "' class='" + options.extraClasses + " " + options.prefix + " " + iconClass + " " + iconSpinClass + " " + iconColorClass + "'></i>";
         },
 
         _setIconStyles: function (img, name) {
